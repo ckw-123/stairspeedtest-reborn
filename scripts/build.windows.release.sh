41fea41a2c83db6c968d1ccfc921aa35eef41411
ckw-123 <98450468+ckw-123@users.noreply.github.com>
2026-02-01 02:16:24 +0800

    Update build script for Windows release
    Change build type to MinSizeRel and add CXX flags.

--- scripts/build.windows.release.sh
+++ scripts/build.windows.release.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -xe
 
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" .
-make -j4
+cmake \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_CXX_FLAGS_MINSIZEREL="$CXX_FLAGS" \
+    -G "Unix Makefiles" \
+    .
+
+make -j
 rm stairspeedtest.exe
 
 # this may change in the future
 export FREETYPE_DEPS=$(pkg-config --libs --static harfbuzz)
 # build resources
 windres -J rc -O coff -i res/res.rc -o res.res
-g++ $(find CMakeFiles/stairspeedtest.dir/src -name "*.obj") "$USERPROFILE/maindeps/curl/lib/libcurl.a" -lbcrypt res.res -o base/stairspeedtest.exe -static -levent -lPNGwriter -lfreetype $FREETYPE_DEPS -lpng -lpcre2-8 -lyaml-cpp -lssl -lcrypto -lws2_32 -lwsock32 -lcrypt32 -liphlpapi -lz -lbz2 -lsecur32 -s
+g++ $LD_FLAGS $(find CMakeFiles/stairspeedtest.dir/src -name "*.obj") "$USERPROFILE/maindeps/curl/lib/libcurl.a" -lbcrypt res.res -o base/stairspeedtest.exe -static -levent -lPNGwriter -lfreetype $FREETYPE_DEPS -lpng -lpcre2-8 -lyaml-cpp -lssl -lcrypto -lws2_32 -lwsock32 -lcrypt32 -liphlpapi -lz -lbz2 -lsecur32
 
 set +xe
